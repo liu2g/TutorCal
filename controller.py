edd3fe1ef26a9070bbe90d622cc3cbf2ebf7dc7c
Liu <liu2z2@mail.uc.edu>
2019-04-05 17:32:47 -0400

    added dynamic directory to adjust to cron

--- controller.py
+++ controller.py
@@ -7,13 +7,14 @@ from email_read import *
 from google_cal import *
 import logging
 import datetime
+import os
 
 #Keep a good log file
-logname=str(datetime.date.today())+'.log'
+logname=os.getcwd()+'/'+str(datetime.date.today())+'.log' #current directory+scanning date+extension
 logging.basicConfig(filename=logname,format='[%(asctime)s]%(levelname)s:%(message)s',level=logging.DEBUG)
 
 #read_appts requests and give list of dict of appoitments, make_events take that and push to google calandar
 #don't forget to make a UNSEEN marker before actual implementation
-logging.info('--------Scanning Period Starts--------')
+logging.info('########Scanning Period Starts########')
 make_events(read_appts())
-logging.info('--------Scanning Period Ends--------')
+logging.info('########Scanning Period Ends########')
